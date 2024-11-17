59d7c4e5a86980d4b8233c45cf527ef438b07731
jellyfin-adult <jellyfinadult@gmail.com>
2024-11-17 16:20:51 +0200

    fix(babepedia): incorrect href extraction (#145)

---
+++ Jellyfin.Plugin.Pronium.Tests/ActorImageProviderTests.cs
@@ -0,0 +1,25 @@
+using Pronium.Helpers.Utils;
+using Pronium.Providers;
+
+namespace Pronium.UnitTests;
+
+[TestFixture]
+public class ActorImageProviderTests
+{
+    [SetUp]
+    public void Setup()
+    {
+        Database.LoadAll();
+    }
+
+    private ActorImageProvider _provider = new();
+
+    [Test, Explicit]
+    [TestCase(TestName = "{c}.{m}")]
+    public async Task GetActorPhotosIsWorking()
+    {
+        var result = await this._provider.GetActorPhotos("Natasha Starr", new CancellationToken());
+
+        Assert.That(result.Count, Is.GreaterThanOrEqualTo(5));
+    }
+}
