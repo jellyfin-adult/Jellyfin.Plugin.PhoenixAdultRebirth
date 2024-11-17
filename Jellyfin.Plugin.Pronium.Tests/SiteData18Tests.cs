7a8b63f9409be482ecee5ba87a0a33df48a27da1
jellyfin-adult <jellyfinadult@gmail.com>
2024-11-17 15:49:59 +0200

    chore(tests): fix data18 tests (#143)

--- Jellyfin.Plugin.Pronium.Tests/SiteData18Tests.cs
+++ Jellyfin.Plugin.Pronium.Tests/SiteData18Tests.cs
@@ -14,13 +14,13 @@ public class SiteData18Tests
     }
 
     private readonly SiteData18 _site = new();
-    private readonly string _testSceneUrl = "https://www.data18.com/scenes/144118";
+    private readonly string _testSceneUrl = "https://www.data18.com/scenes/165843";
 
     [Test]
     [TestCase(TestName = "{c}.{m}")]
     public async Task SearchIsWorking()
     {
-        var result = await _site.Search(new[] { 51, 0 }, "Elite Dyke Society", null, new CancellationToken());
+        var result = await _site.Search(new[] { 51, 0 }, "Slut-Ber Party", null, new CancellationToken());
         Assert.That(result.Count, Is.GreaterThan(0));
         var id = result[0].ProviderIds.Values.FirstOrDefault();
         Assert.That(id, Is.Not.Empty);
@@ -32,14 +32,14 @@ public class SiteData18Tests
     public async Task UpdateIsWorking()
     {
         var result = await _site.Update(new[] { 51, 0 }, new[] { Helper.Encode(_testSceneUrl), "2010-09-14" }, new CancellationToken());
-        Assert.That(result.Item.Name, Is.EqualTo("Elite Dyke Society"));
-        Assert.That(result.Item.OriginalTitle, Is.EqualTo("data18 - 2010-09-14 - Elite Dyke Society"));
+        Assert.That(result.Item.Name, Is.EqualTo("Slut-Ber Party"));
+        Assert.That(result.Item.OriginalTitle, Is.EqualTo("data18 - 2010-09-14 - Slut-Ber Party"));
         Assert.That(result.Item.Overview, Is.Not.Empty);
         Assert.That(result.Item.Studios.Length, Is.EqualTo(3));
-        Assert.That(result.Item.Genres.Length, Is.EqualTo(6));
+        Assert.That(result.Item.Genres.Length, Is.EqualTo(4));
         Assert.That(result.People.Count, Is.EqualTo(2));
-        Assert.That(result.People[0].ImageUrl, Does.Contain("8b1888d6-4d61-4a60-9bec-d9bf3d80c785"));
-        Assert.That(result.People[1].ImageUrl, Does.Contain("7e059593-78ff-40d3-840e-1f5190791c67"));
+        Assert.That(result.People[0].ImageUrl, Does.Contain("Ariella-Ferrera"));
+        Assert.That(result.People[1].ImageUrl, Does.Contain("19ef59a8-18ad-4832-83bd-6d102b40525e"));
     }
 
     [Test]
@@ -70,6 +70,6 @@ public class SiteData18Tests
         Assert.That(result, Has.Count.EqualTo(2));
         Assert.That(
             result.First().Url,
-            Is.EqualTo("https://cdn.dt18.com/media/t/1/scenes/1/2/44118-kelly-divine-sara-jaymes-hot-and-mean.jpg"));
+            Is.EqualTo("https://cdn.dt18.com/media/t/1/scenes/1/3/65843-ariella-ferrera-vanessa-veracruz-hot-and-mean.jpg"));
     }
 }
